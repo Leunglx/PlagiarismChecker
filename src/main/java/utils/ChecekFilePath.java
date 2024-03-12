192ec4cbdc9c3b30593c221df35d7823b25977dd
Leunglx <2644175058@qq.com>
2024-03-12 21:47:07 +0800

    ChecekFilePath类调整

--- src/main/java/utils/ChecekFilePath.java
+++ src/main/java/utils/ChecekFilePath.java
@@ -7,7 +7,7 @@ public class ChecekFilePath {
     public static void pathChecker(String[] pathArr) {
         if(pathArr.length != 3) {
             System.out.println("\n必须提供3个文件路径作为参数。");
-            System.exit(1);
+//            System.exit(1);
         }
         for (String path: pathArr) {
             File file = new File(path);
@@ -15,12 +15,12 @@ public class ChecekFilePath {
                 // 检查文件是否存在
                 if(!file.exists()) {
                     System.err.println("\n文件不存在：" + path);
-                    System.exit(1);
+//                    System.exit(1);
                 }
                 // 检查是不是txt文件
                 if(!path.endsWith(".txt")) {
                     System.out.println("\n存在非txt类型的文件：" + path);
-                    System.exit(1);
+//                    System.exit(1);
                 }
             } catch (Exception e) {
                 throw new RuntimeException(e);
